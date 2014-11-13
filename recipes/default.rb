42c260f7b3c3b41d2f429725787c7e94957ec825
Tom Mitchell <tmitchell@bbn.com>
2014-11-13 15:23:47 -0500

    Download and install gcf tarball

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,28 @@
 # Recipe:: default
 #
 # Copyright (c) 2014 The Authors, All Rights Reserved.
+
+# See http://docs.getchef.com/resource_remote_file.html
+
+# Download branch tkt660_gcfauth of gcf.
+# TODO: when this is released, update to the appropriate download of gcf
+gcf_dir = "/gcf-4094de01de"
+src_url = "http://www.gpolab.bbn.com/experiment-support/gposw/aa/#{gcf_dir}.tar.gz"
+src_checksum = "40664f1cb2b2cbabeabea980507dcddc5ba1ef1dab5a79c2a6eb540909e7afbf"
+src_filepath = "#{Chef::Config[:file_cache_path]}/#{gcf_dir}.tar.gz"
+extract_path = "/opt"
+dest_path = "#{extract_path}/#{gcf_dir}"
+
+remote_file src_filepath do
+  source "#{src_url}"
+  checksum "#{src_checksum}"
+end
+
+
+bash 'extract_module' do
+  code <<-EOH
+    mkdir -p #{extract_path}
+    tar xzf #{src_filepath} -C #{extract_path}
+    EOH
+  not_if { ::File.exists?(dest_path) }
+end
